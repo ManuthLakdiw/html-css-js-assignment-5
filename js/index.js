6a99ffa9f4738740750631d94f7536e0b9fbfae4
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-10 20:35:39 +0530

    added script file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log("js file loaded...");
+const getBtns = document.querySelectorAll("section > div:last-child button");
+const clearbtn = document.querySelector("section > div:first-child button");
+
+
+let lbl = document.querySelector('label');
+let temp = "";
+
+clearbtn.addEventListener("click", () => {
+    lbl.textContent = "";
+    temp = "";
+    console.log("after clear: " + temp);
+});
+
+for (let btn of getBtns) {
+    
+    btn.addEventListener("click", function () {
+
+        if (['+', '-', '*', '/'].includes(btn.innerText)) {
+            temp += lbl.textContent + btn.innerText;
+            lbl.textContent = "";
+       
+        } else if (btn.innerText === "=") {
+            temp += lbl.textContent;
+            const result = eval(temp); 
+            lbl.textContent = result; 
+            temp = ""; 
+           
+        } else {
+            lbl.textContent += btn.innerText;
+        }
+
+    });
+}
